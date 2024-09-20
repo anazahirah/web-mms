067844de4757377f16c21fabf3fad672abf7a2ef
Drigo Alexander Sihombing <73428164+drigoalexander@users.noreply.github.com>
2024-09-21 02:21:52 +0700

    Delete .nuxt directory

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///C:/Users/Terada/web-mms/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/types/content.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '#content/server' {
-  const serverQueryContent: typeof import('C:/Users/Terada/web-mms/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
-  const parseContent: typeof import('C:/Users/Terada/web-mms/node_modules/@nuxt/content/dist/runtime/server').parseContent
-}
